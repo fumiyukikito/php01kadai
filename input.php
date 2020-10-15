b78a83114865785532670389f7c84022bb7aeaa8
kito <69379366+fumiyukikito@users.noreply.github.com>
2020-10-15 22:34:03 +0900

    Create input.php

---
+++ input.php
@@ -0,0 +1,17 @@
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>課題テンプレート</title>
+</head>
+
+<body>
+    <form action="write.php" method="post">
+        お名前: <input type="text" name="name">
+        EMAIL: <input type="text" name="mail">
+        出身: <input type="text" name="place">
+        <input type="submit" value="送信">
+    </form>
+</body>
+
+</html>
